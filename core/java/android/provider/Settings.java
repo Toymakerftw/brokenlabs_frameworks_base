47fe8a55a608098608a672a7280b1613df7c420d
maxwen <max.weninger@gmail.com>
2017-08-27 18:37:00 -0700

    base: Global Actions with Advanced Reboot [1/2]
    With fixes from:
    
    Do not show reboot dialog in lock screen
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Dismiss dialog on screen off
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Vectorized the power menu sound panel selector
    -David Wilson <daveyannihilation@hotmail.com
    
    Fix silent mode icons in new power menu
    Fix airplane mode in new power menu
    -camcory <camcory1@gmail.com>
    
    Pimp out power menu
    -Alex Cruz <mazdarider23@gmail.com>
    
    Powermenu Torch option
    -LorDClockaN <davor@losinj.com>
    
    Add Screen Record to the Power Menu
    -eyosen <abittin@gmail.com>
    
    Power menu customizations [1/2]
    -Dave Kessler <activethrasher00@gmail.com>
    
    [anayw2001 edits: bring up to 11]
    
    Change-Id: I6978be623b0ec7ca9e702fc311449c36b504b1a6
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    (cherry picked from commit 7a858810dc2c2984457a17c4d2385c3b8c0123ab)
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Signed-off-by: neel685 <neel.mandal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4894,6 +4894,12 @@ public final class Settings {
          */
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
+	/**
+	 * Advanced Reboot Toggle
+	 * @hide
+         */
+        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -8970,6 +8976,11 @@ public final class Settings {
          * @hide
          */
         public static final String NOTIFICATION_DISMISS_RTL = "notification_dismiss_rtl";
+
+        /**
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Comma separated list of QS tiles that have been auto-added already.
